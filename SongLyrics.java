3becdea8b8ad55acd19a938cca5db415d65969e6
ljsnell <ljsnell@users.noreply.github.com>
2022-08-26 16:54:46 -0500

    Revert "Added for class demo (#1)" (#2)
    This reverts commit bcb94671434d255b7052619807d0cd00683dffb3.

--- SongLyrics.java
+++ SongLyrics.java
@@ -2,8 +2,7 @@
 public class SongLyrics {
 
 	public static void main(String[] args) {
-		System.out.println("My evil plan to save the world,");
-		System.out.println("Just you wait till it's unfurled");		
+		
 	}
 
 }
