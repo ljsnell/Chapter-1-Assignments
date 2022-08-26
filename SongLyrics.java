bcb94671434d255b7052619807d0cd00683dffb3
ljsnell <ljsnell@users.noreply.github.com>
2022-08-26 16:47:06 -0500

    Added for class demo (#1)
    "My evil plan to save the world" By Five Iron Frenzy

--- SongLyrics.java
+++ SongLyrics.java
@@ -2,7 +2,8 @@
 public class SongLyrics {
 
 	public static void main(String[] args) {
-		
+		System.out.println("My evil plan to save the world,");
+		System.out.println("Just you wait till it's unfurled");		
 	}
 
 }
